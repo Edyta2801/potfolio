3b7af63409f76041c8e4470eca2ab69dec9cbb5a
Edyta2801 <edyta.szarowska@interia.pl>
2020-01-06 10:18:35 +0100

    modify App

--- src/App.js
+++ src/App.js
@@ -1,7 +1,10 @@
 // App.jsx
 
+// App.jsx
+
 import React from 'react';
 import logo from './logo.svg';
+// Import the BrowserRouter, Route and Link components
 import { BrowserRouter, Route, Link } from 'react-router-dom';
 import Projects from './Projects.js';
 import Articles from './Articles.js';
@@ -12,19 +15,21 @@ function App() {
   return (
     <BrowserRouter>
       <div className="App">
-        <Route exact path="/" component={Projects} />
-        <Route path="/articles" component={Articles} />
-        <Route path="/about" component={About} />
         <div className="navigation">
-          <img className="logo-image" src={logo} alt={`Logo Image ${logo}`}/>
           <div className="navigation-sub">
             <Link to="/" className="item">Projects</Link>
             <Link to="/articles" className="item">Articles</Link>
             <Link to="/about" className="item">About</Link>
           </div>
         </div>
+        <div>
+          <img src={logo} className="logo-image" alt="Logo Image" />
+        </div>
+        <Route exact path="/" component={Projects} />
+        <Route path="/articles" component={Articles} />
+        <Route path="/about" component={About} />
       </div>
-    </BrowserRouter>
+    </BrowserRouter >
   );
 }
 
