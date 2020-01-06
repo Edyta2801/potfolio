feaa3ac9f176817901b53bb28d00e3a0b5a27a6c
Edyta2801 <edyta.szarowska@interia.pl>
2020-01-06 09:59:44 +0100

    add example logo

--- src/App.js
+++ src/App.js
@@ -1,22 +1,30 @@
 // App.jsx
 
 import React from 'react';
-// Replace SVG file with your logo
 import logo from './logo.svg';
+import { BrowserRouter, Route, Link } from 'react-router-dom';
+import Projects from './Projects.js';
+import Articles from './Articles.js';
+import About from './About.js';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <div className="navigation">
-        <img src={logo} className="logo" alt="Logo Image" />
-        <div className="navigation-sub">
-          <a href="" className="item">Projects</a>
-          <a href="" className="item">Articles</a>
-          <a href="" className="item">About</a>
+    <BrowserRouter>
+      <div className="App">
+        <Route exact path="/" component={Projects} />
+        <Route path="/articles" component={Articles} />
+        <Route path="/about" component={About} />
+        <div className="navigation">
+          <img className="logo-image" src={logo} alt={`Logo Image ${logo}`}/>
+          <div className="navigation-sub">
+            <Link to="/" className="item">Projects</Link>
+            <Link to="/articles" className="item">Articles</Link>
+            <Link to="/about" className="item">About</Link>
+          </div>
         </div>
       </div>
-    </div>
+    </BrowserRouter>
   );
 }
 
