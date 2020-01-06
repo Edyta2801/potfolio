fc4a7e3664810c42014a279f203e6f09cfc45ae7
Edyta2801 <edyta.szarowska@interia.pl>
2020-01-06 10:18:25 +0100

    add Thumbnails

--- src/Projects.js
+++ src/Projects.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import Thumbnail from './Thumbnail.js'; 
+import Thumbnail from './Thumbnail.js';
 import './App.css';
 
 function Projects(props) {
@@ -11,6 +11,26 @@ function Projects(props) {
                 image=""
                 title=""
                 category=""
+            />
+            <Thumbnail
+                link="/twitter"
+                image="http://twitter-image-url.jpg"
+                title="Twitter Newsfeed"
+                category="Mobile App"
+            />
+
+            <Thumbnail
+                link="/airbnb"
+                image="http://airbnb-image-url.jpg>"
+                title="Airbnb Experiences"
+                category="Website"
+            />
+
+            <Thumbnail
+                link="/photoshop"
+                image="http://photoshop-image-url.jpg"
+                title="Photoshop Redesign"
+                category="Desktop App"
             />
         </div>
     )
