185df38558f15a2c18227642c304fd8d72813903
Edyta2801 <edyta.szarowska@interia.pl>
2020-01-06 22:50:32 +0100

    modify code

--- src/App.js
+++ src/App.js
@@ -1,36 +1,33 @@
-// App.jsx
-
-// App.jsx
-
-import React from 'react';
+import React, { Component } from 'react';
 import logo from './logo.svg';
-// Import the BrowserRouter, Route and Link components
 import { BrowserRouter, Route, Link } from 'react-router-dom';
 import Projects from './Projects.js';
 import Articles from './Articles.js';
 import About from './About.js';
 import './App.css';
 
-function App() {
-  return (
-    <BrowserRouter>
-      <div className="App">
-        <div className="navigation">
-          <div className="navigation-sub">
-            <Link to="/" className="item">Projects</Link>
-            <Link to="/articles" className="item">Articles</Link>
-            <Link to="/about" className="item">About</Link>
-          </div>
+class App extends Component {
+  render() {
+    return (
+      <BrowserRouter>
+        <div className="App">
+          <section className="header">
+            <div className="navigation">
+              <div className="home"><a href="Edyta2801">home</a></div>
+
+              <Link to="/" className="item">Projects</Link>
+              <Link to="/articles" className="item">Articles</Link>
+              <Link to="/about" className="item">About</Link>
+              <img src={logo} className="App-logo" alt="logo" />
+              <Route exact path="/" component={Projects} />
+              <Route path="/articles" component={Articles} />
+              <Route path="/about" component={About} />
+            </div>
+          </section>
         </div>
-        <div>
-          <img src={logo} className="logo-image" alt="Logo Image" />
-        </div>
-        <Route exact path="/" component={Projects} />
-        <Route path="/articles" component={Articles} />
-        <Route path="/about" component={About} />
-      </div>
-    </BrowserRouter >
-  );
+      </BrowserRouter >
+    );
+  };
 }
 
 export default App;
