a9cbc130cd2945e86650d241dda2657064e60df9
Edyta2801 <edyta.szarowska@interia.pl>
2020-01-07 14:39:44 +0100

    modify App with bug

--- src/App.js
+++ src/App.js
@@ -1,33 +1,39 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import { BrowserRouter, Route, Link } from 'react-router-dom';
-import Projects from './Projects.js';
-import Articles from './Articles.js';
-import About from './About.js';
-import './App.css';
+import React from 'react';
+import { BrowserRouter, Route } from 'react-router-dom';
+import Projects from './components/views/Projects/Projects.js';
+import Contact from './components/views/Contact/Contact.js';
+import About from './components/views/About/About.js';
+import styles from './App.scss';
+import { AnimatedSwitch } from 'react-router-transition';
+import MainLayout from './components/layout/MainLayout/MainLayout.js'
+import Home from './components/views/Home/Home.js';
 
-class App extends Component {
+
+
+class App extends React.Component {
   render() {
     return (
       <BrowserRouter>
-        <div className="App">
-          <section className="header">
-            <div className="navigation">
-              <div className="home"><a href="Edyta2801">home</a></div>
-
-              <Link to="/" className="item">Projects</Link>
-              <Link to="/articles" className="item">Articles</Link>
-              <Link to="/about" className="item">About</Link>
-              <img src={logo} className="App-logo" alt="logo" />
-              <Route exact path="/" component={Projects} />
-              <Route path="/articles" component={Articles} />
-              <Route path="/about" component={About} />
-            </div>
-          </section>
-        </div>
-      </BrowserRouter >
+        <MainLayout>
+          <AnimatedSwitch
+            atEnter={{ opacity: 0, offset: 200 }}
+            atLeave={{ opacity: 0 }}
+            atActive={{ opacity: 1, offset: 0 }}
+            className={styles.switchWrapper}
+            mapStyles={styles => ({
+              opacity: styles.opacity,
+              transform: `translateY(${styles.offset}px)`,
+            })}
+          >
+            <Route exact path="/" component={Home} />
+            <Route exact path="/projects" component={Projects} />
+            <Route exact path="/contact" component={Contact} />
+            <Route exact path="/about" component={About} />
+          </AnimatedSwitch>
+        </MainLayout>
+      </BrowserRouter>
     );
-  };
+  }
 }
 
 export default App;
